b58ab2baa9872b6f7cbe0a665790e67d97c19a94
Sarwani-Mallavarapu <sarwani.mallavarapu@quantela.com>
2026-04-12 14:46:51 +0530

    Create python_basics_Sarwani.py

---
+++ python_basics_Sarwani.py
@@ -0,0 +1,25 @@
+# Task 1: Variables and Data Types Create variables to store a person's name (string), age (integer), height in meters (float), and whether they are a student (boolean). Print all four values in a single print() statement.
+name="Sarwani"
+age=26
+height=160.7
+isStudent= True
+
+
+
+# Task 2: String Formatting Using the variables from Task 1, print a sentence in the following format: Name: <name> | Age: <age> | Height: <height> | Student: <is_student>
+
+print("Name: ",name," | Age: ", age, " | Height: ",height," | Student: ",isStudent)
+
+'''
+Task 3: Arithmetic Operations Using the age variable, calculate and print:
+
+Age in months
+Age in days (assume 365 days/year)
+The remainder when age is divided by 7
+Age raised to the power of 2
+'''
+print(age*12)
+print(age*365)
+print(age/7)
+print(age**2)
+
